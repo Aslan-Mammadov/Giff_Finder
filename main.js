75efc41eacfee46f99093d1dca88673fd38da00b
Aslan <aslan.mammadov96@gmail.com>
2024-02-07 18:55:42 -0500

    create variables

---
+++ main.js
@@ -0,0 +1,9 @@
+const baseUrl='https://api.giphy.com';
+const apiKey='YWV6cQZuVWJwjMabuBcxhlqZzqSf5DLk';
+const searchBtn=document.querySelector('.js-btn')
+const container=document.querySelector('.js-container');
+let userSearch=document.querySelector('.js-search');
+let inputNumber=document.querySelector('.js-number');
+
+
+
